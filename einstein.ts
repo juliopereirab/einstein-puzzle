90673229b65bf8d7cd928737ba4ecb2d50d90725
Julio Pereira <juliopereira@julios-mbp.home>
2022-10-23 09:54:03 +0100

    fix: setting arrays can and cant be

--- einstein.ts
+++ einstein.ts
@@ -6,48 +6,68 @@
 // position: 1, 2, 3, 4, 5
 
 
-// The Brit lives in the red house
+// The Brit lives in the red houseValue
 // The Swede keeps dogs as pets
 // The Dane drinks tea
-// The green house is on the left of the white house
+// The green houseValue is on the left of the white houseValue
 // The green house’s owner drinks coffee
 // The person who smokes Pall Mall rears birds
-// The owner of the yellow house smokes Dunhill
-// The man living in the center house drinks milk
-// The Norwegian lives in the first house
+// The owner of the yellow houseValue smokes Dunhill
+// The man living in the center houseValue drinks milk
+// The Norwegian lives in the first houseValue
 // The man who smokes Blends lives next to the one who keeps cats
 // The man who keeps horses lives next to the man who smokes Dunhill
 // The owner who smokes BlueMaster drinks beer
 // The German smokes Prince
-// The Norwegian lives next to the blue house
+// The Norwegian lives next to the blue houseValue
 // The man who smokes Blend has a neighbor who drinks water
 
 // The question is, who owns the fish?
 
 //npx ts-node einstein.ts
 
+type Nationality = "Brit" | "Swede" | "Dane" | "Norwegian" | "German";
+type Colour = "green" | "red" | "yellow" | "blue" | "white";
+type Drink = "tea" | "water" | "milk" | "beer" | "coffee";
+type Pet = "fish" | "dogs" | "birds" | "cats" | "horses";
+type Cigarrette = "Pallmall" | "Prince" | "Blend" | "Bluemaster" | "Dunhill";
 
-interface house {
-    nationality?: "Brit" | "Swede" | "Dane" | "Norwegian" | "German";
-    colour?: "green" | "red" | "yellow" | "blue" | "white";
-    drink?: "tea" | "water" | "milk" | "beer" | "coffee";
-    pet?: "fish" | "dogs" | "birds" | "cats" | "horses";
-    cigarrette?: "Pallmall" | "Prince" | "Blend" | "Bluemaster" | "Dunhill";    
+interface Option<T>{
+    value?: T;
+    canBe?: T[];
+    cantBe?: T[];
+}
+
+interface houseValue{
+    nationality?: Nationality
+    colour?: Colour
+    drink?: Drink
+    pet?: Pet
+    cigarrette?: Cigarrette
+    isCorner?: boolean;
+}
+
+interface House {
+    nationality?: Option<Nationality>
+    colour?: Option<Colour>
+    drink?: Option<Drink>
+    pet?: Option<Pet>
+    cigarrette?: Option<Cigarrette>  
     isCorner?: boolean;
 }
 
 
-// const orderedElements = [
-//     {nationality: "Norwegian"}, 
-//     {colour: "blue"},
-//     {drink: "milk"},
-//     undefined,
-//     undefined   
-// ] as (house | undefined)[]
+const orderedElements : House[] = [
+    {nationality: {value: "Norwegian"}, isCorner: true}, 
+    {colour: {value: "blue"}},
+    {drink: {value: "milk"}},
+    {},
+    {isCorner: true}   
+] 
 
 const keyNames = ["nationality", "colour", "cigarrette", "drink", "pet"]
 
-const looseElements : house[] = [
+const looseElements : houseValue[] = [
     {nationality: "Brit", colour: "red"},
     {nationality: "Swede", pet: "dogs"},
     {nationality: "Dane", drink: "tea"},
@@ -58,7 +78,7 @@ const looseElements : house[] = [
     {nationality: "German", cigarrette: "Prince"}
 ]
 
-const relations : house[][] = [
+const relations : houseValue[][] = [
     [{colour: "green"},{colour: "white"}],
     [{pet: 'cats'},{cigarrette: "Blend"}],
     [{cigarrette: "Dunhill"},{pet: "horses"}], 
@@ -66,7 +86,38 @@ const relations : house[][] = [
     [{nationality: "Norwegian", isCorner: true}, {colour: "blue"}, {drink: "milk"}]
 ];
 
-function enrichLooseElements(looseElements: house[]){
+function HouseToHouseValue(house: House){
+    const values = (Object.entries(house)).map(([k, v]) => ({[k]: v.value}))
+    return values.length > 0
+        ? values.reduce((c, n) => ({...c, ...n})) as houseValue
+        : {};
+}
+
+function complete(){
+    for(var i=0; i<orderedElements.length; i++){
+        var house = orderedElements[i];
+        var neighbours = [orderedElements[i-1], orderedElements[i+1]]
+        for(var keyName of keyNames.filter(k => !house[k]?.value)){
+            const consideredRelations = relations.filter(r => {
+                const keys = Object.keys(r.reduce((c, n) => ({...c, ...n})));
+                return keys.indexOf(keyName) !== -1;
+            })
+            consideredRelations.forEach(r => {
+                for(var suggestion of r){
+                    if(!housesMatch(HouseToHouseValue(house), suggestion) && !neighbours.some(n => !!n && housesMatch(HouseToHouseValue(n), suggestion))){
+                       if(!house[keyName]?.canBe) house[keyName] = {canBe: []};
+                       house[keyName].canBe.push(suggestion[keyName])
+                    } else {
+                        if(!house[keyName]?.cantBe) house[keyName] = {cantBe: []};
+                        house[keyName].cantBe.push(suggestion[keyName]);
+                    }
+                }
+            })
+        }
+    }
+}
+
+function enrichLooseElements(looseElements: houseValue[]){
     for(var i=0; i < looseElements.length; i++){
         let el = looseElements[i];
         for(var ii = looseElements.indexOf(el)+1; ii < looseElements.length; ii++){
@@ -81,11 +132,11 @@ function enrichLooseElements(looseElements: house[]){
     }
 }
 
-function housesAreRelated(h1: house, h2: house){
+function housesAreRelated(h1: houseValue, h2: houseValue){
     return Object.values(h1).some(v => Object.values(h2).indexOf(v) !== -1);
 }
 
-function enrichRelations(looseElements: house[], relations: house[][]){
+function enrichRelations(looseElements: houseValue[], relations: houseValue[][]){
     const mappedElements = looseElements.map(el => ({[Object.values(el).join(",")]: el}))
         .reduce((c, n) => ({...c, ...n}));
 
@@ -107,11 +158,11 @@ function enrichRelations(looseElements: house[], relations: house[][]){
     }
 }
 
-function housesMatch(h1: house, h2: house){
+function housesMatch(h1: houseValue, h2: houseValue){
     return Object.values(h1).some(v => Object.values(h2).indexOf(v) !== -1);
 }
 
-function relateRelations(relations: house[][]){
+function relateRelations(relations: houseValue[][]){
     for(var i=0; i < relations.length; i++){
         const relation = relations[i];
         for(var ii = relations.indexOf(relation)+1; ii < relations.length; ii++){
@@ -126,55 +177,55 @@ function relateRelations(relations: house[][]){
                        relation[h1] = merged;
                        comparison[h2] = merged;
                     }
-
-                    let mainHouse = relation[h1];
-                    let neighBours = relation.filter(h => h !== mainHouse);
-
-                    for(var k of keyNames){
-                        
-                    }
+                    // let mainHouse = relation[h1];
+                    // let neighBours = relation.filter(h => h !== mainHouse);
+                    // for(var k of keyNames){
+                    // }
                 }
             }
         }
     }
 }
 
-const housesDontMatch = (h1: house, h2: house) => {
+const housesDontMatch = (h1: houseValue, h2: houseValue) => {
     const fields = Object.keys({...h1, ...h2});
 
     return fields.some(t => h1[t] && h2[t] && h1[t] !== h2[t])
 }
 
-function positionIsDiscarded(r1: house[], r2:house[]){
+// function positionIsDiscarded(r1: houseValue[], r2:houseValue[]){
 
-    for(var i=0; i < r1.length; i++){
-        if(r1[i] && r2[i] && housesDontMatch(r1[i], r2[i])) return true;
-    }
+//     for(var i=0; i < r1.length; i++){
+//         if(r1[i] && r2[i] && housesDontMatch(r1[i], r2[i])) return true;
+//     }
 
-    return false;
-}
+//     return false;
+// }
 
-function compareRelations(r1: house[], r2:house[]){
-    const base = r1.length > r2.length ? r1 : r2;
-    const comparison = base === r1 ? r2 : r1;
+// function compareRelations(r1: houseValue[], r2:houseValue[]){
+//     const base = r1.length > r2.length ? r1 : r2;
+//     const comparison = base === r1 ? r2 : r1;
 
-    // for(var i=0; i< base.length; i++){
-    //     if()
-    // }
+//     // for(var i=0; i< base.length; i++){
+//     //     if()
+//     // }
 
-}
+// }
 
 let loops = 0;
+
+// console.log(HouseToHouseValue(orderedElements[3]))
 
 while(loops < 30){
     enrichLooseElements(looseElements);
     enrichRelations(looseElements, relations);
     relateRelations(relations);
+    complete();
     loops++;
 }
 
 console.log("looseElements", looseElements);
 console.log("relations", relations);
-
+console.log("orderedElements", JSON.stringify(orderedElements[0]));
 
 
